7c88b811a1bf6aa07861e13328ca12e11cb26386
LISi-cmd-coder <87555178+LISi-cmd-coder@users.noreply.github.com>
2021-08-07 22:25:52 -0300

    Delete tarea2 directory

--- tarea2/tarea2.js
+++
@@ -1,7 +0,0 @@
-      var texto = document.getElementById('txt');
-      var contar = document.getElementById('txtvista');
-      
-      texto.addEventListener('keyup', function(){
-          contar.innerText = texto.value.length;
-
-      })
